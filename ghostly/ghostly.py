b0c8b825361ea70114cbe4d705e6fe1536c8f413
Alex Hayes <alex@acommoncreative.com>
2015-11-30 13:52:57 +1100

    Removed non-tested "selector" methods in preference for xpath only interface.

--- ghostly/ghostly.py
+++ ghostly/ghostly.py
@@ -1,31 +1,20 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+    ghostly.ghostly
+    ~~~~~~~~~~~~~~~
+
+    Lightweight wrapper and helpers around Selenium Webdriver.
 
 """
-Surely we can do
-basic acceptance testing
-with just 7 commands
-(& 5 asserts)
-
-Browser Commands: get, click, fill, submit, wait, switch_to, navigate
-Asserts: assert_text, assert_element, assert_value, assert_title, assert_url
-"""
-
-import random
-import string
+from __future__ import absolute_import, print_function, unicode_literals
 import time
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, \
-    ElementNotVisibleException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
 
 from .errors import DriverDoesNotExistError, GhostlyTestFailed, \
     GhostlyTimeoutError
-
-
-def milli_now():
-    return int(time.time() * 1000)
 
 
 class Ghostly:
@@ -51,80 +40,11 @@ class Ghostly:
     def end(self):
         self.driver.quit()
 
-    def _get_element(self, selector, parent=None, wait=10):
-        """
-        Returns a single element using a stripped down version of the jQuery lookup
-        functions. Options are:
-            - #element_id
-            - .element_class
-            - element_name
-            - Link Text
-        """
-
-        # using our own wait-for-element logic
-        wait = wait * 1000
-        start = milli_now()
-
-        # disable the driver/selenium wait time so that we can use our own logic
-        self.driver.implicitly_wait(0)
-
-        element = None
-        while milli_now() < start + wait:
-            if not parent:
-                parent = self.driver
-
-            try:
-                if selector.startswith('#'):
-                    elements = parent.find_elements_by_id(selector.replace('#', ''))
-                elif selector.startswith('.'):
-                    elements = parent.find_elements_by_class_name(selector.replace('.', ''))
-                else:
-                    funcs = [
-                        parent.find_elements_by_tag_name,
-                        parent.find_elements_by_name,
-                        parent.find_elements_by_id,
-                        parent.find_elements_by_css_selector,
-                        parent.find_elements_by_link_text
-                    ]
-
-                    for f in funcs:
-                        try:
-                            elements = f(selector)
-                            if elements:
-                                break
-                        except NoSuchElementException:
-                            pass
-
-                if elements:
-                    for element in elements:
-                        # ignore hidden form elements
-                        if element.tag_name.lower() == 'input' and element.get_attribute('type') == 'hidden':
-                            continue
-                        return element
-
-            except NoSuchElementException:
-                pass
-
-        raise NoSuchElementException('Could not find element matching {}'.format(selector))
-
     def get(self, url):
         """
         Load the provided URL in the web driver
         """
         return self.driver.get(url)
-
-    def click(self, selector):
-        """
-        Click on an element that's currently visible on the page.
-
-        The element can be selected with a range of selectors:
-            - .class_name
-            - #element_id
-            - element
-            - "Link Text"
-        """
-        element = self._get_element(selector)
-        element.click()
 
     def xpath_click(self, xpath, wait=0.1, move_to=True):
         """
@@ -211,31 +131,6 @@ class Ghostly:
         """
         return self.driver.find_element_by_xpath(xpath)
 
-    def submit(self, selector, *contents):
-        """
-        Fill out and submit a form
-        """
-        form = self.fill(selector, *contents)
-        return form.submit()
-
-    def fill(self, selector, *contents):
-        """
-        Fill out a form without submitting it.
-
-        Provide a list where the first item is the selector to use to find the form and
-        all following items are <selector>: <value> pairs to be used to complete the form.
-
-        Use the `submit` function if you also want to submit the form.
-        """
-        r = ''.join(random.choice(string.letters + string.digits) for _ in range(12))
-        form = self._get_element(selector)
-        for c in contents:
-            for k, v in c.items():
-                v = v.replace('<random>', r)
-                element = self._get_element(k, parent=form)
-                element.send_keys(v)
-        return form
-
     def wait(self, seconds):
         """
         Wait for a specified number of seconds
@@ -243,63 +138,3 @@ class Ghostly:
         if type(seconds) == str:
             seconds = int(seconds)
         time.sleep(seconds)
-
-    def switch_to(self, selector):
-        """
-        Switch to a new frame (useful for navigating between iFrames)
-        """
-        self.driver.switch_to.frame(selector)
-
-    def navigate(self, navigation):
-        """
-        Possible navigation methods:
-            - forward
-            - back
-        """
-        if navigation not in ['forward', 'back']:
-            raise AttributeError("An invalid option was given to the navigation command")
-        f = getattr(self.driver, navigation)
-        f()
-
-    def assert_text(self, text, selector='body'):
-        """
-        Assert that a piece of text exists on the currently displayed page.
-        """
-        self.wait(1)
-        element = self._get_element(selector)
-
-        if text not in element.text:
-            raise GhostlyTestFailed("{} not in {}".format(text, element.text))
-
-    def assert_element(self, selector):
-        """
-        Ensure that at least one element exists that matches the selector
-        """
-        self.wait(1)
-        element = self._get_element(selector)
-        if not element:
-            raise GhostlyTestFailed("no element matched {}".format(selector))
-
-    def assert_value(self, selector, value):
-        """
-        Assert that the value of a form element is the provided value
-
-        - assert_value:
-          - "input"
-          - "Hello World"
-        """
-        self.wait(1)
-        element = self._get_element(selector)
-        if element.get_attribute('value') != value:
-            raise GhostlyTestFailed("{} != {}".format(element.get_attribute('value'), value))
-
-    def assert_title(self, value):
-        self.wait(1)
-        if self.driver.title != value:
-            raise GhostlyTestFailed("title is {} not {}".format(self.driver.title, value))
-
-    def assert_url(self, url):
-        self.wait(1)
-        if self.driver.current_url != url:
-            raise GhostlyTestFailed("url is {} not {}".format(self.driver.current_url, url))
-
